a62b83ee669f98d3bccbdca6126ff3a353408fb8
afornerot <afornerot@cadoles.com>
2026-07-12 14:01:09 +0200

    Redirect /login to dex when OIDC mode

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use Jumbojett\OpenIDConnectClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,8 +14,25 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
+    public function login(
+        Request $request,
+        AuthenticationUtils $authenticationUtils,
+        ParameterBagInterface $parameterBag,
+    ): Response {
+        if ($parameterBag->get('appModeAuth') === 'OIDC') {
+            $oidc = new OpenIDConnectClient(
+                $parameterBag->get('oidcIssuer'),
+                $parameterBag->get('oidcClientId'),
+                $parameterBag->get('oidcClientSecret')
+            );
+            $oidc->setVerifyPeer(false);
+            $oidc->setVerifyHost(false);
+            $oidc->setRedirectURL($parameterBag->get('oidcRedirectUri'));
+            $oidc->addScope(['openid', 'email', 'profile']);
+
+            $oidc->authenticate();
+        }
+
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
